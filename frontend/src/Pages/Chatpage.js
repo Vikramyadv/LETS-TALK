b39e48329bf685cbd511daccf1568252f45e6f72
Vikram Yadav <vikramyadav1402@gmail.com>
2023-08-05 16:26:18 +0530

    first commit

---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,32 @@
+import { Box } from "@chakra-ui/layout";
+
+import { ChatState } from "../Context/ChatProvider";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import ChatBox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
+import { useState } from "react";
+
+const Chatpage = () => {
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
+};
+
+export default Chatpage;
---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: process.env.PORT_URL,
+      changeOrigin: true,
+    })
+  );
+};
